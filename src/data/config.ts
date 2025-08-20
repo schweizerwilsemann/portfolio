61af6e5c939aa07c9157de4813e9f291b0f7f7db
KhoaND <khoa.qianyinga@gmail.com>
2025-08-21 01:33:59 +0700

    push project

---
+++ src/data/config.ts
@@ -0,0 +1,64 @@
+const config = {
+  title: "Khoa Nguyen | Web Developer",
+
+  description: {
+    long: "Explore the portfolio of Khoa, a student at University of Transport and Communications with a passion for web development. I am interested in frontend and backend development, as well as devops and cloud computing. I am also interested in solving real life problems using technology.",
+    short:
+      "Discover the portfolio of Khoa, a student at University of Transport and Communications with a passion for web development.",
+  },
+  keywords: [
+    "Khoa",
+    "Khoa Nguyen",
+    "khoanguyen",
+    "khoanguyen.com",
+    "Khoa Nguyen portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    "React",
+    "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "Google",
+    "Google Play",
+    "Firebase",
+    "DevOps",
+    "AIOps",
+    "Azure",
+    "Linux",
+    "Shell",
+    "Python",
+    "Git",
+    "Prettier",
+    "NPM",
+    "Wordpress",
+    "Nginx",
+    "Vim",
+    "Bash",
+    "Vercel",
+  ],
+  author: "Khoa Nguyen",
+  email: "khoa.qianyinga@gmail.com",
+  site: "https://github.com/schweizerwilsemann",
+
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/schweizer_wilse",
+    linkedin: "https://www.linkedin.com/in/khoa-nguyen-02672a241/",
+    facebook: "https://www.facebook.com/khoa.qianyinga",
+    github: "https://github.com/schweizerwilsemann/",
+    instagram: "https://www.instagram.com/_aohkcud/",
+
+  },
+};
+export { config };
